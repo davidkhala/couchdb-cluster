6cbaa228a02066623a3f6fd191173501bc8c202f
davidLiu <david-khala@hotmail.com>
2019-01-30 17:47:33 +0800

    [TODO] why only 1 port is allowed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # couchdb-cluster
 container-based couchdb cluster sample
+
+
+# TODO
+- why only 1 couchdb work
--- bootstrap.js
+++ bootstrap.js
@@ -1,20 +1,22 @@
-const {imagePull, containerStart, containerDelete, networkRemove, networkCreate} = require('./docker-manager/nodejs/dockerode-util');
+const {imagePull, containerStart, containerDelete, networkRemove, networkCreateIfNotExist} = require('./docker-manager/nodejs/dockerode-util');
 const {setNetwork, setPortBind} = require('./docker-manager/nodejs/containerOptsBuilder');
 const config = {
 	couchdb1: {
-		port: 5984
+		port: 6984
 	},
 	couchdb2: {
-		port: 6984
+		port: 7984
 	}
 };
 const network = 'couchNetwork';
 const Image = 'couchdb:latest';
 const start = async () => {
 	await imagePull(Image);
-	await networkCreate({Name: network}, false);
+	await networkCreateIfNotExist({Name: network}, false);
 	for (const [name, configEntry] of Object.entries(config)) {
-		let containerOpts = {name, Image};
+		let containerOpts = {
+			name, Image
+		};
 		containerOpts = setNetwork(containerOpts, network, [name]);
 		containerOpts = setPortBind(containerOpts, `${configEntry.port}:5984`);
 
